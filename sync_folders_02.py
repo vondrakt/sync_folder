4f1581d08cc2276a2d3a5be8a761d4148d22b187
Tihana Vondrak <51750652+vondrakt@users.noreply.github.com>
2023-08-03 20:52:35 +0200

    Add files via upload

--- sync_folders_02.py
+++ sync_folders_02.py
@@ -2,8 +2,8 @@
 
 from optparse import OptionParser
 import os
-import time
-import sqlite3
+import pandas
+import hashlib
 
 # defining the arguments that need to be passed to the script
 arguments = OptionParser()
@@ -19,14 +19,27 @@ if (options.source is None or options.replica is None or options.period is None)
     arguments.print_help()  # and provide the help
     exit(-1)  # exit the script
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def calculate_checksum(file):
+    with open(file, 'rb') as f:
+        data = f.read()
+        md5_checksum = hashlib.md5(data).hexdigest()
+        return(md5_checksum)
 
-# Create a sqlite database with all the files from source.
-source_database = sqlite3.connect('source_info.db')
-source_database_cursor = source_database.cursor()
+# The tables need to have columns: file ID, timestamp, checksum
 
-table_query = '''
-filename TEXT NOT NULL,
-path TEXT NOT NULL,
-size INTEGER,
-modified INTEGER
-'''
+# Creating a pandas table that will contain source files and their file metadata
+source_df = None
+
+source_files = [options.source + '/' + i for i in os.listdir(options.source)]
+for i in source_files:
+    row = {'file ID': i, 'timestamp': os.path.getmtime(i), 'checksum': calculate_checksum(i)}
+    #print(row)
+    if source_df is None:
+        source_df = pandas.DataFrame(row, index=[0])
+        #print(source_df)
+    else:
+        row_df = pandas.DataFrame(row, index=[0])
+        source_df = pandas.concat([source_df, row_df], ignore_index=True)
+
+print(source_df.head())
+
